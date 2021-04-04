ff2a5de997d4b51b7e41256e87535e336abdf5ad
unimatrix2 <dnny.nis@gmail.com>
2021-04-04 03:01:19 -0300

    added board page

---
+++ pages/board.jsx
@@ -0,0 +1,22 @@
+import { Context } from '../context/Context';
+import MainAppBar from '../components/AppBar';
+import { useContext, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import { useRouter } from 'next/router';
+
+export default function Board() {
+	const { state } = useContext(Context);
+	const router = useRouter();
+	useEffect(() => {
+		if (!state.user?.nickname) {
+			router.push('/');
+		}
+	}, [])
+
+	return (
+		<>
+			<MainAppBar />
+			<Typography>{JSON.stringify(state)}</Typography>
+		</>
+	)
+}
